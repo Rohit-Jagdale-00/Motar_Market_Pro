1f2497d463af9da0ad818b7ea31af753e7c11cb1
rohitjagdale-00 <rohitjagdale0606@gmail.com>
2025-08-19 18:45:51 +0530

    Final

--- routing-app/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    {path: '',redirectTo: 'hello',pathMatch: 'full'},
-    {path: 'hello',loadComponent: () => import('./hello/hello').then(m => m.Hello)},
-    {path: 'signal',loadComponent: () => import('./signal/signal').then(m => m.Signal)}
-];
